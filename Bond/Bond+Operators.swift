e4daefefc30b5626f6c931ffdb21634b59197d25
Tadeas Kriz <tadeas@brightify.org>
2015-08-13 18:12:45 +0200

    Allow binding optionals.

--- Bond/Bond+Operators.swift
+++ Bond/Bond+Operators.swift
@@ -34,53 +34,62 @@ infix operator <->> { associativity left precedence 100 }
 
 // MARK: Bind and fire
 
-public func ->> <T>(left: Observable<T>, right: Bond<T>) {
-  left.bindTo(right)
+public func ->> <T>(left: Observable<T>?, right: Bond<T>?) {
+  if let right = right {
+    if let left = left {
+      right.bind(left)
+    } else {
+      right.unbind()
+    }
+  }
 }
 
-public func ->> <T>(left: Observable<T>, right: Dynamic<T>) {
-  left ->> right.valueBond
+public func ->> <T>(left: Observable<T>?, right: Dynamic<T>?) {
+  left ->> right?.valueBond
 }
 
-public func ->> <T: Dynamical, U where T.DynamicType == U>(left: T, right: Bond<U>) {
-  left.designatedDynamic ->> right
+public func ->> <T: Dynamical, U where T.DynamicType == U>(left: T?, right: Bond<U>?) {
+  left?.designatedDynamic ->> right
 }
 
-public func ->> <T: Dynamical, U: Bondable where T.DynamicType == U.BondType>(left: T, right: U) {
-  left.designatedDynamic ->> right.designatedBond
+public func ->> <T: Dynamical, U: Bondable where T.DynamicType == U.BondType>(left: T?, right: U?) {
+  left?.designatedDynamic ->> right?.designatedBond
 }
 
-public func ->> <T, U: Bondable where U.BondType == T>(left: Observable<T>, right: U) {
-  left ->> right.designatedBond
+public func ->> <T, U: Bondable where U.BondType == T>(left: Observable<T>?, right: U?) {
+  left ->> right?.designatedBond
 }
 
 // MARK: Bind only
-
-public func ->| <T>(left: Observable<T>, right: Bond<T>) {
-  right.bind(left, fire: false)
+public func ->| <T>(left: Observable<T>?, right: Bond<T>?) {
+  if let right = right {
+    if let left = left {
+      right.bind(left, fire: false)
+    } else {
+      right.unbind()
+    }
+  }
 }
 
-public func ->| <T>(left: Observable<T>, right: T -> Void) -> Bond<T> {
+public func ->| <T>(left: Observable<T>?, right: T -> Void) -> Bond<T> {
   let bond = Bond<T>(right)
-  bond.bind(left, fire: false)
+  left ->| bond
   return bond
 }
 
-public func ->| <T: Dynamical, U where T.DynamicType == U>(left: T, right: Bond<U>) {
-  left.designatedDynamic ->| right
+public func ->| <T: Dynamical, U where T.DynamicType == U>(left: T?, right: Bond<U>?) {
+  left?.designatedDynamic ->| right
 }
 
-public func ->| <T: Dynamical, U: Bondable where T.DynamicType == U.BondType>(left: T, right: U) {
-  left.designatedDynamic ->| right.designatedBond
+public func ->| <T: Dynamical, U: Bondable where T.DynamicType == U.BondType>(left: T?, right: U?) {
+  left?.designatedDynamic ->| right?.designatedBond
 }
 
-public func ->| <T, U: Bondable where U.BondType == T>(left: Observable<T>, right: U) {
-  left ->| right.designatedBond
+public func ->| <T, U: Bondable where U.BondType == T>(left: Observable<T>?, right: U?) {
+  left ->| right?.designatedBond
 }
-
 
 // MARK: Two way bind
-
 public func <->> <T>(left: Dynamic<T>, right: Dynamic<T>) {
   left.bindTo(right.valueBond, fire: true, strongly: true)
   right.bindTo(left.valueBond, fire: false, strongly: false)
